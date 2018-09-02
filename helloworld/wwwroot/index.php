0e6f0d7c25f8995c01bcd941ec45908e71548d96
Luis San Martin <luis@sanmartin.io>
2018-09-02 23:25:48 +0200

    spaces removed

--- helloworld/wwwroot/index.php
+++ helloworld/wwwroot/index.php
@@ -1,7 +1,5 @@
 <?php
-
 require_once 'dbconfig.php';
-
 $dsn = "pgsql:host=$host;port=5432;dbname=$db;user=$username;password=$password";
 
 try{
@@ -11,7 +9,9 @@ try{
  // display a message if connected to the PostgreSQL successfully
  if($conn){
  echo "Connected to the <strong>$db</strong> database successfully!";
- echo "\n";
+ echo "<br>";
+ echo "<br>";
+ echo "<br>";
 
  // retrieve data
 
@@ -27,65 +27,64 @@ try{
  echo $e->getMessage();
 }
 
-
  $redisObj = new Redis();
 
-  function openRedisConnection( $hostName, $port){
+  function openRedisConnection($hostName, $port){
   	global $redisObj;
 	// Opening a redis connection
-  	$redisObj->connect( $hostName, $port );
+  	$redisObj->connect($hostName, $port );
   	return $redisObj;
   }
 
-  function setValueWithTtl( $key, $value, $ttl ){
+  function setValueWithTtl($key, $value, $ttl){
 
   	try{
   		global $redisObj;
 		// setting the value in redis
-  		$redisObj->setex( $key, $ttl, $value );
+  		$redisObj->setex($key, $ttl, $value);
   	}catch( Exception $e ){
   		echo $e->getMessage();
   	}
   }
 
-  function getValueFromKey( $key ){
+  function getValueFromKey($key){
   	try{
   		global $redisObj;
 		// getting the value from redis
-  		return $redisObj->get( $key);
+  		return $redisObj->get($key);
   	}catch( Exception $e ){
   		echo $e->getMessage();
   	}
   }
 
-  function deleteValueFromKey( $key ){
+  function deleteValueFromKey($key){
   	try{
   		global $redisObj;
 		// deleting the value from redis
-  		$redisObj->del( $key);
+  		$redisObj->del($key);
   	}catch( Exception $e ){
   		echo $e->getMessage();
   	}
   }
-
 
   // Making json string
   $jsonValue = json_encode($row);
 
   // Opening a redis connection
-  openRedisConnection( 'redis.default.svc.cluster.local', 6379 );
+  openRedisConnection('redis.default.svc.cluster.local', 6379);
 
   // Inserting the value with ttl =  1 hours
-  setValueWithTtl( 'somekey1', $jsonValue, 3600);
+  setValueWithTtl('somekey1', $jsonValue, 3600);
 
   // Fetching value from redis using the key.
-  $val = getValueFromKey( 'somekey1' );
+  $val = getValueFromKey('somekey1');
 
   //  Output:  the json encoded array from redis
   echo "<strong> output from redis</strong>";
-  echo "\n";
+  echo "<br>";
+  echo "<br>";
   echo $val;
 
   // Unsetting value from redis
-  deleteValueFromKey( $key );
+  deleteValueFromKey($key);
 ?>
